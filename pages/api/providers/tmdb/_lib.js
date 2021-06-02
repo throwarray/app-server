7b4cc5ea23750e6fba2f2c5890118a03c70e66d1
throwarray <throwarray@users.noreply.github.com>
2021-06-01 21:03:57 -0400

    remove deprecated request dependency

--- pages/api/providers/tmdb/_lib.js
+++ pages/api/providers/tmdb/_lib.js
@@ -1,4 +1,6 @@
-const request = require('request')
+const fetch = require('isomorphic-unfetch')
+
+const { URL } = require('url')
 
 const auth = {
     v3: process.env.TMDB_V3,
@@ -29,12 +31,6 @@ function config (props) {
 }
 
 function Search ({ type = 'movie', ...opts }, cb) {
-    const options = {
-        qs: Object.assign({ api_key: auth.v3 }, defaultSearchOpts, opts),
-        method: 'GET',
-        json: true
-    }
-
     if (!type || !opts.query) cb(new Error('Invalid query'))
     
     else {
@@ -49,11 +45,10 @@ function Search ({ type = 'movie', ...opts }, cb) {
         if (!url) cb(new Error('Invalid query'))
     
         else {
-            options.url = url
-
-            request(options, function (error, response) {
-                cb(error, response)
-            })
+            fetchJSON({
+                url,
+                qs: Object.assign({ api_key: auth.v3 }, defaultSearchOpts, opts)
+            }, cb)
         }
     }
 }
@@ -64,21 +59,10 @@ function Meta ({
     type,
     ...opts 
 }, cb) {
-    const options = {
-        qs: Object.assign({ 
-            api_key: auth.v3,
-            language: 'en-US',
-            append_to_response: 'videos,images,external_ids,content_ratings,keywords,credits'
-        }, opts),
-        method: 'GET',
-        json: true
-    }
-
     if (!type) cb(new Error('Invalid query'))
     
     else {
         let url, tmdbId = Number(id)
-
 
         switch (type) {
             case 'movie': 
@@ -89,42 +73,40 @@ function Meta ({
         }
 
         if (Number.isNaN(tmdbId) || !url) cb(new Error('Invalid query'))
-        
-        else {
-            options.url = url
 
-            request(options, function (error, response) {
-                cb(error, response)
-            })
+        else {
+            fetchJSON({
+                url,
+                qs: Object.assign({ 
+                    api_key: auth.v3,
+                    language: 'en-US',
+                    append_to_response: 'videos,images,external_ids,content_ratings,keywords,credits'
+                }, opts)
+            }, cb)
         }
     }
 }
 
 // Handle meta for season / episode
-function Season ({ id, season, episode, ...opts }, cb) 
-{
+function Season ({ id, season, episode, ...opts }, cb) {
     const seasonNumber = Number(season)
 
     const tvId = Number(id)
-    
+
     const options = {
         url: `https://api.themoviedb.org/3/tv/${tvId}/season/${seasonNumber}`,
         qs: Object.assign({ 
             api_key: auth.v3,
             language: 'en-US',
             append_to_response: 'videos,images,external_ids,content_ratings,keywords,credits'
-        }, opts),
-        method: 'GET',
-        json: true
+        }, opts)
     }
 
     if (episode !== void 0) options.url = options.url + '/episode/' + Number(episode)
 
-    if (Number.isNaN(seasonNumber) || Number.isNaN(tvId)) cb(new Error('Invalid query'))
+    if (Number.isNaN(seasonNumber) || Number.isNaN(tvId)) cb(new Error('Invalid Request'))
     
-    else request(options, function (error, response) {
-        cb(error, response)
-    })
+    else fetchJSON(options, cb)
 }
 
 function Discover ({ type, ...opts }, cb) {
@@ -136,26 +118,30 @@ function Discover ({ type, ...opts }, cb) {
         case 'tv': url = 'https://api.themoviedb.org/3/discover/tv'; break
     }
 
-    if (!url) { 
-        cb(new Error('Invalid query'))
-        
+    fetchJSON({ url, qs: { ...defaultSearchOpts, api_key: auth.v3, ...opts } }, cb)
+}
+
+
+function fetchJSON ({ url, qs = {}, method = 'GET', json, ...otherProps }, cb) {
+    let route, iterable
+
+    try {
+        route = new URL(url)
+
+        // Support Map and URLSearchParams
+        iterable = qs.__proto__.entries? qs.entries() : Object.entries(qs)
+    
+        // Add query params
+        for (let [key, value] of iterable) { route.searchParams.set(key, value) }
+    } catch(e) {
+        cb(new Error('Invalid Request'))
         return
     }
-    
-    const qs = { 
-        ...defaultSearchOpts,
-        api_key: auth.v3,
-        ...opts
-    }
 
-    const options = {
-        url,
-        qs,
-        method: 'GET',
-        json: true
-    }
-
-    request(options, function (error, response) {
-        cb(error, response)
-    })
+    // Fetch JSON and return response
+    (async function () {
+        const response = await fetch(route, { method, ...otherProps })
+        const body = await response.json()
+        cb(void 0, { response, body })
+    })().catch((error)=> cb(error))
 }
